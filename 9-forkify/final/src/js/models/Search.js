95092aa34fc0de492f57fe54e77829e619c48979
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-06-30 18:24:14 -0400

    Object structure changes after calling a method

--- 9-forkify/final/src/js/models/Search.js
+++ 9-forkify/final/src/js/models/Search.js
@@ -3,12 +3,16 @@ import { key, proxy } from '../config';
 
 export default class Search {
     constructor(query) {
-        this.query = query;
+        this.query = query;   // ***  When an object of this class is created, it will start out with just one property, this.query ***
     }
 
     async getResults() {
         try {
             const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            
+            // **************** Stores the result in the Search object **************************************************************
+            //                  so the Search object will have a new property called this.result after this method has been called
+            // **********************************************************************************************************************
             this.result = res.data.recipes;
             // console.log(this.result);
         } catch (error) {
